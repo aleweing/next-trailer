713d3775aa58837c9e4220c54171e995c3c5bae4
aleweing <alewein@proton.me>
2026-06-19 21:37:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'next-show-v1';
+const CACHE_NAME = 'next-show-v2';
 const STATIC_ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
